ece3d09953f4288063421d390b260b84aca6b61a
Calli <me@calli.dev>
2021-12-07 21:32:07 +0100

    made day 7 solution always work

--- 7/day7.hs
+++ 7/day7.hs
@@ -4,9 +4,7 @@ import GHC.Float (int2Float, float2Int)
 -- by taking the first line of the file, replacing all , to spaces,
 -- splitting on spaces using words and then mapping all the words to integers
 getListInput :: String -> IO [Int]
-getListInput path = do
-    content <- readFile path
-    return $ map read $ words $ map f $ head $ lines content
+getListInput path = readFile path >>= return . map read . words . map f . head . lines
     where f ',' = ' '
           f x = x
 
@@ -25,6 +23,10 @@ calcDistFuel n = (n * (n + 1)) `div` 2
 calcMean :: [Int] -> Int
 calcMean xs = sum xs `div` length xs
 
-main = do
-    input <- getListInput "./input.txt"
-    print $ calcFuel input $ calcMean input
+-- calculate the minimum fuel used by taking the mean and then looking at the 3 integers surrounding it
+calcMinimumFuel :: [Int] -> Int
+calcMinimumFuel xs = minimum [calcFuel xs $ m - 1, calcFuel xs m, calcFuel xs $ m + 1]
+    where m = calcMean xs
+
+main = getListInput "./input.txt" >>= print . calcMinimumFuel
+
